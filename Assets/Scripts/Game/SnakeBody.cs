664673597d14d815697e2ae4c9ebffbe871ca87e
exitshadow <exertingshadows@gmail.com>
2022-07-18 16:12:13 +0200

    GrowSnake() is operational and is called upon collision. all very cute.

--- Assets/Scripts/Game/SnakeBody.cs
+++ Assets/Scripts/Game/SnakeBody.cs
@@ -7,13 +7,20 @@ using UnityEngine;
     /// </summary>
 
 // todo
-//  => use Tail.cs positions management functions to translate it
+// [x] use Tail.cs positions management functions to translate it
 //      to the points that will generate or update a new mesh
-//  => use the positions to generate bones and skin them to the slice's
+//  * use the positions to generate bones and skin them to the slice's
 //      vertices (by adapting the existing GenerateMesh() function)
-//  => split controls from Snake.cs to SnakeController.cs that will only
-//      manage the player's controls on the snake and leave the body logic
-//      to this script.
+//  * split controls from Snake.cs to SnakeController.cs
+//      that will only manage the player's controls on the snake and
+//      leave the body logic to this script.
+
+// TODO = Dynamically growing the snake
+//  might need a List<OrientedPoint> and List<float>...
+//  in fact it's more complicated than that because they cannot be modified
+//  by reference, might need to find a trick with a wider array and swap values
+//  on the go depending on the size (ie a variable like currentLength)
+//  ... seems feasible it just gives the snake a maximum length
 
 [RequireComponent(typeof(MeshFilter))]
 [RequireComponent(typeof(MeshRenderer))]
@@ -24,7 +31,8 @@ public class SnakeBody : MonoBehaviour
     [SerializeField] bool preview = true;
     [SerializeField] [Range(.5f, 1f)] float bodyThickness = .5f;
     [SerializeField] Transform[] thicknessModulator = new Transform[4];
-    [SerializeField] int initialSegmentsCount = 20;
+    [SerializeField] int initialSegmentsCount = 10;
+    [SerializeField] int maxSegmentsCount = 50;
     [SerializeField] float segmentsInterval = .5f;
     [SerializeField] [Range(1, 3)] int subDivs = 1; // * might not be needed anymore
 
@@ -40,25 +48,61 @@ public class SnakeBody : MonoBehaviour
     // * testing purposes only
     [SerializeField] LineRenderer linePreview;
 
-    // todo might need a List<OrientedPoint> instead
+
     private OrientedPoint[] segmentPoints;
     // NOTE OrientedPoint struct contains
     //      position, rotation and velocity
+    private float[] thicknessMapping; // could add that to OrientedPoint :thinking:
+    private Mesh mesh;
+    private int vc;
+    private ushort step;
+    private ushort stop;
+    private int currentSegmentsCount;
 
+    void Awake()
+    {
+        vc = shape.VertCount;
+        currentSegmentsCount = initialSegmentsCount;
+
+        if (shape.isSmooth)
+        {
+            step = 1;
+            stop = 1;
+        }
+        else
+        {
+            step = 2;
+            stop = 0;
+        }
+
+        if (shape.isSymmetrical)
+        {
+            for (int i = 0; i < vc; i++)
+            {
+                shape.baseVertices[i].c = (shape.baseVertices[i].point.y + 1) / 2f;
+            }
+        }
+
+        mesh = new Mesh();
+        mesh.name = "Snake Body";
+        GetComponent<MeshFilter>().sharedMesh = mesh;
+
+    }
 
     void Start()
     {
         // * testing purposing only
         linePreview.positionCount = initialSegmentsCount;
 
-        segmentPoints = new OrientedPoint[initialSegmentsCount];
+        segmentPoints = new OrientedPoint[maxSegmentsCount];
     }
 
     void Update()
     {
+        // this is equivalent to the old PopulateInitialPositions()
         segmentPoints[0].position = head.position;
         linePreview.SetPosition(0, segmentPoints[0].position); //*testing only
-        for (int i = 1; i < segmentPoints.Length; i++)
+        for (int i = 1; i < currentSegmentsCount; i++)
         {
             Vector3 target = segmentPoints[i-1].position;
             Vector3 current = segmentPoints[i].position;
@@ -73,5 +117,21 @@ public class SnakeBody : MonoBehaviour
             // * testing only
             linePreview.SetPosition(i, segmentPoints[i].position);
         } 
+    }
+
+    private void GenerateBodyMesh() {
+        Debug.Log("Starting mesh generation");
+    }
+
+    public void GrowSnake() {
+        if (currentSegmentsCount < maxSegmentsCount)
+        {
+            Debug.Log("Grow the snake");
+            linePreview.positionCount++; // * testing only
+            currentSegmentsCount++;
+            GenerateBodyMesh();
+        }
+        else Debug.Log("Snake has attained its maximum length.");
+        
     }
 }
--- Assets/Scripts/Game/SnakeController.cs
+++ Assets/Scripts/Game/SnakeController.cs
@@ -1,18 +1,30 @@
+using System.Runtime.Serialization;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class SnakeController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] float movingSpeed = 3f;
+    [SerializeField] float steeringSpeed = 200f;
+    [SerializeField] Transform body;
+
+    void OnTriggerEnter(Collider other)
     {
-        
+        Debug.Log("Collision Enter!");
+        if (other.CompareTag("GrowObject"))
+        {
+            body.GetComponent<SnakeBody>().GrowSnake();
+        }
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        transform.position += transform.forward * movingSpeed * Time.deltaTime;
+
+        float h_steerDirection = Input.GetAxis("Horizontal");
+        float v_SteerDirection = Input.GetAxis("Vertical");
+        transform.Rotate(Vector3.up * h_steerDirection * steeringSpeed * Time.deltaTime);
+        transform.Rotate(Vector3.left * v_SteerDirection * steeringSpeed * Time.deltaTime);
     }
 }
