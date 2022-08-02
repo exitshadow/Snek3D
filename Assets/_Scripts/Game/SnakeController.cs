5c51ade6db8987bc6e5c932c657cb756596cb2dd
exitshadow <exertingshadows@gmail.com>
2022-08-02 16:35:47 +0200

    adding some juiciness, splash screen and physics cherries

--- Assets/_Scripts/Game/SnakeController.cs
+++ Assets/_Scripts/Game/SnakeController.cs
@@ -5,9 +5,16 @@ using UnityEngine;
 
 public class SnakeController : MonoBehaviour
 {
+    [SerializeField] Camera mainCamera;
     [SerializeField] float movingSpeed = 3f;
     [SerializeField] float steeringSpeed = 200f;
     [SerializeField] Transform body;
+    [SerializeField] Transform rotationAxis;
+
+    private Rigidbody rb;
+    private Vector3 screenPos;
+    float h_steerDirection;
+    float v_SteerDirection;
 
     void OnTriggerEnter(Collider other)
     {
@@ -21,12 +28,23 @@ public class SnakeController : MonoBehaviour
         }
     }
 
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
     void Update()
     {
+        rotationAxis.position = transform.position;
+        rotationAxis.rotation = mainCamera.transform.rotation;
+
         transform.position += transform.forward * movingSpeed * Time.deltaTime;
 
-        float h_steerDirection = Input.GetAxis("Horizontal");
-        float v_SteerDirection = Input.GetAxis("Vertical");
+        h_steerDirection = Input.GetAxis("Horizontal");
+        v_SteerDirection = Input.GetAxis("Vertical");
+
+        screenPos = mainCamera.WorldToScreenPoint(transform.position);
+
         transform.Rotate(Vector3.up * h_steerDirection * steeringSpeed * Time.deltaTime);
         transform.Rotate(Vector3.left * v_SteerDirection * steeringSpeed * Time.deltaTime);
     }
